4c4b9cbe00eb6ab6ff90ceb724779b55f7d99c0d
riasat <11375716+crimsonvspurple@users.noreply.github.com>
2022-04-14 00:33:42 +0600

    monospace font and basic game layout (text)

--- lib/src/base_game/game_board_view.dart
+++ lib/src/base_game/game_board_view.dart
@@ -2,18 +2,39 @@ import 'dart:ui';
 
 import 'package:c4ai/src/base_game/game_board_service.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../settings/settings_view.dart';
 
-class GameBoardView extends StatelessWidget {
-  GameBoardView({Key? key}) : super(key: key);
+var bigText = GoogleFonts.getFont("Xanh Mono").copyWith(fontSize: 90);
+const medText = TextStyle(fontFeatures: [FontFeature.tabularFigures()], fontSize: 45);
+
+class GameBoardView extends StatefulWidget {
+  const GameBoardView({Key? key}) : super(key: key);
 
   static const routeName = '/';
 
-  final GameBoardService gameBoardService = GameBoardService();
+  @override
+  State<GameBoardView> createState() => _GameBoardViewState();
+}
+
+class _GameBoardViewState extends State<GameBoardView> {
+  late final GameBoardService gameBoardService;
+  late Player player1;
+  late Player player2;
+
+  @override
+  void initState() {
+    super.initState();
+    gameBoardService = GameBoardService();
+    player1 = Player(Piece.max, "MAX");
+    player2 = Player(Piece.min, "MIN");
+  }
 
   @override
   Widget build(BuildContext context) {
+    Board board = gameBoardService.playPiece(ROW.values.last, COL.zero, player1);
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('c4ai'),
@@ -31,9 +52,19 @@ class GameBoardView extends StatelessWidget {
         ),
         body: Padding(
             padding: const EdgeInsets.all(16),
-            child: Text(
-              gameBoardService.printBoard(),
-              style: const TextStyle(fontFeatures: [FontFeature.tabularFigures()], fontSize: 90),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text("P: " + player1.name + " W: " + player1.streak.toString(), style: medText),
+                    const Spacer(),
+                    Text("P: " + player2.name + " W: " + player2.streak.toString(), style: medText),
+                    const Spacer(),
+                    Text("Move: " + gameBoardService.getMoveCount().toString(), style: medText)
+                  ],
+                ),
+                Text(gameBoardService.printBoard(), style: bigText)
+              ],
             )));
   }
 }
